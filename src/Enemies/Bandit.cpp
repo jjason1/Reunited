f86c87a846859097a3b8704558123c1374f82409
akhan521 <aamirksfg@gmail.com>
2023-11-13 10:23:40 -0800

    adding bandit tests

--- src/Enemies/Bandit.cpp
+++ src/Enemies/Bandit.cpp
@@ -1,6 +1,15 @@
 #include "../../lib/Enemies/Bandit.hpp"
 
-Bandit::Bandit(string name,int level) : Enemy::Enemy(name,level){}
+Bandit::Bandit(string name,int level) : Enemy::Enemy(name,level){
+    //Setting our stat increments:
+    int hpInc,defInc,atkInc;
+    hpInc = defInc = atkInc = 5;
+    this->setIncrmnts(hpInc,defInc,atkInc);
+    //Base stats:
+    this->setHealth(150); 
+    this->setDefense(80);
+    this->setAttack(25);
+}
 
 /*
 void Bandit::stun(Character *c){
--- test/testBanditClass.cpp
+++ test/testBanditClass.cpp
@@ -7,3 +7,37 @@ TEST(BanditClassSuite, testNewBandit){
     string name="Aamir";
     EXPECT_NO_THROW(new Bandit(name, level));
 }
+
+TEST(BanditClassSuite, testNoNameBandit){
+    int level=1;
+    string name="";
+    EXPECT_DEATH(new Bandit(name, level), "Your name must not be empty!");
+}
+
+TEST(BanditClassSuite, testInvalidLevelBandit){
+    int level=-1;
+    string name="Ryan";
+    EXPECT_DEATH(new Bandit(name, level), "Your level must not be less than 0!");
+}
+
+/*
+TEST(BanditClassSuite, testNormalAttack){
+    //Arrange.
+    Bandit *b=new Bandit("Aamir",1);
+    Character *c=new Character("Ryan");
+    //Act.
+    b->normalAttack(c);
+    //Assert.
+    EXPECT_EQ(c->getHealth(),75);
+}
+
+TEST(BanditClassSuite, testStun){
+    //Arrange.
+    Bandit *b=new Bandit("Jason",1);
+    Character *c=new Character("Boyi");
+    //Act.
+    b->stun(c);
+    //Assert.
+    EXPECT_TRUE(c->checkMovable() == false);
+}
+*/
