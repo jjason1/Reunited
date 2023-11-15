c9debc426ce63118416f260be936e7a7203c4449
akhan521 <aamirksfg@gmail.com>
2023-11-14 22:10:11 -0800

    starting an enemy team class

--- src/Enemies/EnemyTeam.cpp
+++ src/Enemies/EnemyTeam.cpp
@@ -1,27 +1,3 @@
 #include "../../lib/Enemies/EnemyTeam.hpp"
 
-EnemyTeam::EnemyTeam(vector<Enemy *> enemyList){
-    this->enemyList=enemyList;
-}
 
-void EnemyTeam::setSize(int size){
-    this->numEnemies=size;
-}
-
-int EnemyTeam::getSize(){
-    return this->numEnemies;
-}
-
-void EnemyTeam::addEnemy(Enemy *e){
-    this->enemyList.push_back(e);
-}
-
-Character * EnemyTeam::chooseTarget(vector<Character *> team,int indx){
-    //If the provided team is empty...
-    if(team.size()==0) return nullptr;
-    //Returning a specific character ptr.
-    if(indx>=0 && indx<team.size()){
-        return team[indx];
-    }
-    else return nullptr;
-}
