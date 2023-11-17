9921c3cc740d84a774bc07666ddd25c3fb2c32dd
akhan521 <aamirksfg@gmail.com>
2023-11-17 15:24:10 -0800

    adding some tests

--- lib/Enemies/EnemyTeam.hpp
+++ lib/Enemies/EnemyTeam.hpp
@@ -7,11 +7,9 @@
 
 class EnemyTeam {
     private:
-        int numEnemies;
         vector<Enemy *> enemyList;
     public:
         EnemyTeam(vector<Enemy *>);
-        void setSize(int);
         int getSize();
         Character * chooseTarget(vector<Character *>,int);
         void addEnemy(Enemy *);
--- src/Enemies/EnemyTeam.cpp
+++ src/Enemies/EnemyTeam.cpp
@@ -1,3 +1,1 @@
 #include "../../lib/Enemies/EnemyTeam.hpp"
-
-
