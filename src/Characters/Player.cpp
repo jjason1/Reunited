f9db4f860ac466812f46ffd01b4161714f2e370b
bli181 <bli181@ucr.edu>
2023-11-22 00:30:18 -0800

    Test 1 done

--- src/Characters/Player.cpp
+++ src/Characters/Player.cpp
@@ -1,21 +1,21 @@
-#include "Player.hpp"
+#include "../../lib/Characters/Player.hpp"
 #include <iostream>
 
 using namespace std;
 
-Player::Player(string name):Character(name),atkDstrbtn(0) {
-
+Player::Player(string name, int level):Character(name),atkDstrbtn(0) {
+    assert(level>=0 && "Your level must not be less than 0!");
 }
 
 //List all the skills
 void Player::listSkills(ostream &out){ 
-    for(int size_s = 0; i < skills.size(); ++i){
+    for(int i = 0; i < skills.size(); ++i){
         out << i+1 << ". " <<  skills[i] << endl;
     }
 }
 
 //Skill selection function for override
-void Player::selectSkill(const sstring &skillName) {
+void Player::selectSkill(const string &skillName) {
     for (const string& skill : skills) {
         if (skill == skillName) {
             currentSkill = skillName;
--- test/testPlayerClass.cpp
+++ test/testPlayerClass.cpp
@@ -1,3 +1,32 @@
-#include "../src/Characters/Player.cpp"
-#include "gtest/gtest.h"
 
+#include "gtest/gtest.h"
+#include "../src/Characters/Player.cpp"
+#include <iostream>
+#include <ostream>
+
+TEST(TestPlayerClassSuite, TestNewPlayer){
+    int level=1;
+    string name = "Boyi";
+    EXPECT_NO_THROW(new Player(name, level));
+}
+
+TEST(TestPlayerClassSuite, TestNoNamePlayer){
+    int level=1;
+    string name = "";
+    EXPECT_DEATH(new Player(name, level), "Your name must not be empty!");
+}
+
+TEST(TestPlayerClassSuite, TestInvalidLevelPlayer){
+    string name = "Boyi";
+    int level=-1;
+    EXPECT_DEATH(new Player(name, level), "Your level must not be less than 0!");
+}
+
+/* Need a mock test
+TEST(TestPlayerClassSuite, TestPrintSkiils){
+    Player *P = new Player("Aamir", 1);
+    P->skills = {'Fireball', 'Icestorm'};
+    P->listSkills;
+    EXPECT_EQ();
+}
+*/
