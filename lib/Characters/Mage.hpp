9b1f5960a1821a8b18045a92726f2c4317a122e9
jjason1 <Jason33771@gmail.com>
2023-11-23 21:32:15 -0800

    Inital commits for Mage class

---
+++ lib/Characters/Mage.hpp
@@ -0,0 +1,19 @@
+#include "Character.hpp"
+#include "../Enemies/Enemy.hpp"
+
+#include <iostream>
+using namespace std;
+
+class Mage : public Character
+{
+private:
+    int manaBar;
+
+public:
+    Mage(string);
+    void fireBall(Enemy *);
+    void healing();
+    void iceStorm(Enemy *);
+    void skillBoost();
+    void thunderBlast(Enemy *);
+}
