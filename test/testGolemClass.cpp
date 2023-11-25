84caf9dd171e91f9d1b2d536545cdc64d8ed742e
bli181 <bli181@ucr.edu>
2023-11-24 21:12:56 -0800

    Finished Test

--- test/testGolemClass.cpp
+++ test/testGolemClass.cpp
@@ -12,3 +12,30 @@ TEST(GolemClassSuite, testNoNameGolem){
     string name="";
     EXPECT_DEATH(new Golem(name, level), "Your name must not be empty!");
 }
+
+TEST(GolemClassSuite, testInvalidLevelGolem){
+    int level = -1;
+    string name="Aamir";
+    EXPECT_DEATH(new Golem(name, level), "Your level must not be less than 0!");
+}
+
+
+TEST(GolemClassSuite, testNormalAttack){
+    Golem *g = new Golem("Boyi" , 1);
+    Character *c = new Character("Jason");
+    c->setHealth(100);
+    g->normalAttack(c);
+    EXPECT_EQ(c->getHealth(), 50);
+}
+
+TEST(GolemClassSuite, testShieldGen){
+    Golem *g = new Golem("Boyi" , 1);
+    g->shieldGen();
+    EXPECT_EQ(g->getDefense(), 1500);
+}
+
+TEST(GolemClassSuite, testSelectSkill){
+    Golem *g = new Golem("Ryan" , 1);
+    int chosenSkill = g->selectSkill();
+    EXPECT_TRUE((chosenSkill==1)||(chosenSkill==2));
+}
