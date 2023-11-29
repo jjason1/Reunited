bf72d7acc9472f1a763a17d2e20c6ae87344d8b3
akhan521 <aamirksfg@gmail.com>
2023-11-28 21:45:25 -0800

    adding final tests

--- src/Enemies/EnemyTeam.cpp
+++ src/Enemies/EnemyTeam.cpp
@@ -4,11 +4,14 @@ EnemyTeam::EnemyTeam(vector<Character *> enemies, ostream &out) :
     CharacterTeam::CharacterTeam(enemies,out){}
 
 Character *EnemyTeam::chooseTarget(vector<Character *> players){
-    if(players.size()==0) return nullptr;
+    if(players.size()==0){
+        out<<"No targets are available."<<endl;
+        return nullptr;
+    }
     //Selecting a random target.
     int target=rand()%players.size();
     //Displaying the player being targetted.
-    out<<players[target]->getName()<<" is being targetted by the enemy team."<<endl;
+    out<<players[target]->getName()<<" is being targeted by the enemy team."<<endl;
     //Returning the target from the vector of players.
     return players[target];
 }
