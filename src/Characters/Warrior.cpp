e2eabb1a4f94bc92d3ba693fde7f9008ce7c9d99
bli181 <bli181@ucr.edu>
2023-11-29 18:29:05 -0800

    Added resetHealth function

---
+++ src/Characters/Warrior.cpp
@@ -0,0 +1,12 @@
+#include"../Characters/Warrior.hpp"
+#include<iostream>
+#include<string>
+
+class Warrior(string name) : Character::Character(name) {
+    
+        void strike(Enemy *enemy){
+
+
+        }
+    
+};
