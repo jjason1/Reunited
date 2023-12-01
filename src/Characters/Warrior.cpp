1931c992940ef8ab9449685e4039af745e9043b4
bli181 <bli181@ucr.edu>
2023-11-30 19:13:20 -0800

    Still working

--- src/Characters/Warrior.cpp
+++ src/Characters/Warrior.cpp
@@ -16,21 +16,53 @@ Warrior::Warrior(string name) : Player::Player(name){
     this->setHealth(baseStats);
     this->setDefense(baseStats);
     this->setAttack(baseStats);
+}
 
     //Strike is normal attack.
-    void Warrior::strike(Enemy *e) {
-        cout << this->getName() << " used strikee on " << e->getName() << "." << endl;
-        e->takeDamage(this->getAttack());
-    }
-
-    void Warrior::heavyStrike(Enemy *e) {
-        double multiplier = 1.5;
-        cout << this->getName() << " used heavyStrike on " << e->getName() << "." << endl;
-        e->takeDamage(this->getAttack() * multiplier);
-    }
-
-    void Warrior::warCry() {
-        int warCry = 2;
-        this->setAttack(warCry * baseStats);
-    }
+void Warrior::strike(Enemy *e) {
+    cout << this->getName() << " used strikee on " << e->getName() << "." << endl;
+    e->takeDamage(this->getAttack());
 }
+
+void Warrior::heavyStrike(Enemy *e) {
+    //Only bonus after strike(Not implemented)
+    double multiplier = 1.5;
+    cout << this->getName() << " used heavy strike on " << e->getName() << "." << endl;
+    e->takeDamage(this->getAttack() * multiplier);
+}
+
+void Warrior::warCry() {
+    int warCry = 2;
+    this->setAttack(warCry * this->getAttack());
+    cout << this->getName() << " used War Cry, you gained double attack in next three terms." << endl;
+}
+
+void Warrior::stormStrike(Enemy *e) {
+    //Only bonus after heavy strike(Not implemented)
+    int bonus = 2;
+    cout << this->getName() << " used storm strike on " << e->getName() << "." << endl;
+    e->takeDamage(this->getAttack() * bonus);
+}
+
+void Warrior::bloodDraining() {
+    double drain = 0.2;
+    //in next 6 turns(Not implemented):
+    this->setHealth(this->getHealth() + drain * this->getAttack());
+}
+
+void Warrior::rageBurst() {
+    //Only can when rage bar is half or above and cosume half of rage bar after using skill(Not implemented)
+
+}
+
+void Warrior::taunt() {
+    //Not implemented yet.
+}
+
+void Warrior::heartOfAWarrior() {
+    //in 5 turns:
+    this->setHealth(1.2 * (baseStats + this->getlevel() * hpIncrmnt))
+    
+}
+
+    
