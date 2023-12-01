42044962b7c25c587a5ab5943c688a16024c3802
Ulrixon <ryan70235@gmail.com>
2023-12-01 15:54:22 -0800

    create Chapter hpp and need to first finish SubChapter and then continue the work here

---
+++ lib/Chapters/Chapter.hpp
@@ -0,0 +1,15 @@
+#ifndef CHAPTER_HPP
+#define CHAPTER_HPP
+#include "../Characters/Player.hpp"
+#include <istream>
+#include <ostream>
+#include <string>
+class Chapter {
+private:
+  std::ostream &out;
+  std::istream &in;
+  Player *player;
+  string input;
+};
+
+#endif
