6523b3a2f20079932589f0ad9afc24ce2ff5677c
Aamir Khan <aamirksfg@gmail.com>
2023-12-01 19:41:03 -0800

    Update Combat.hpp

--- lib/Combat.hpp
+++ lib/Combat.hpp
@@ -7,32 +7,33 @@
 #include <ostream>
 
 class Combat {
-private:
-  int turns;
-  PlayerTeam *OriginalPlayerTeam; // store the original health and name of player
-  PlayerTeam *playerTeam;
-  PlayerTeam *outPlayerTeam; // store players that is already has hp<=0
-  EnemyTeam *OriginalEnemyTeam; // store the original health and name of enemy
-  EnemyTeam *enemyTeam;
-  EnemyTeam *outEnemyTeam; // store enemies that is already has hp<=0
-  ostream &out;
-  istream &in;
+    private:
+      int turns;
+      PlayerTeam *OriginalPlayerTeam; // store the original team 
+      PlayerTeam *playerTeam;         // store the active team
+      PlayerTeam *outPlayerTeam;    // store defeated players
+      EnemyTeam *OriginalEnemyTeam; // store the original enemy team 
+      EnemyTeam *enemyTeam;
+      EnemyTeam *outEnemyTeam; // store defeated enemies
+      ostream &out;
+      istream &in;
 
-public:
-  Combat(PlayerTeam *, EnemyTeam *, ostream &, istream &);
-  void startBattle();
-  int showCurrentTurn();
-  bool checkBattleEnd(); // check if either team all have been eliminated
-  void resetCombat(); // reset combat when restart the combat
-  bool chooseRestart(); // activated after defeated, choose escape the fight and
-                        // pretend winning or restart the fight
-  void showAllCharactersStatus();
-  PlayerTeam *getPlayerTeam();
-  PlayerTeam *getOutPlayerTeam();
-  PlayerTeam *getOriginalPlayerTeam();
-  EnemyTeam *getEnemyTeam();
-  EnemyTeam *getOutEnemyTeam();
-  EnemyTeam *getOriginalEnemyTeam();
+    public:
+      Combat(PlayerTeam *, EnemyTeam *, ostream &, istream &);
+      void startBattle();
+      int showCurrentTurn();
+      bool checkBattleEnd(); // check if either team all have been eliminated
+      void resetCombat();    // reset combat when restart the combat
+      bool chooseRestart();  // activated after defeated, choose escape the fight and
+                             // pretend winning or restart the fight
+      void showAllCharactersStatus();
+      PlayerTeam *getPlayerTeam();
+      PlayerTeam *getOutPlayerTeam();
+      PlayerTeam *getOriginalPlayerTeam();
+      EnemyTeam *getEnemyTeam();
+      EnemyTeam *getOutEnemyTeam();
+      EnemyTeam *getOriginalEnemyTeam();
+
 };
 
 #endif
