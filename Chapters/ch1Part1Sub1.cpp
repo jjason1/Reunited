7d53cffa6f2a3c9f4a04fdc96eb092b3c10cf639
akhan521 <aamirksfg@gmail.com>
2023-12-03 11:56:03 -0800

    fixing text formatting and readability

--- Chapters/ch1Part1Sub1.cpp
+++ Chapters/ch1Part1Sub1.cpp
@@ -16,32 +16,32 @@ using std::string;
 
 
 void Chapter1Part1::jobDescription() {
-  out << "w-Warrior" << endl;
-  out << "Description: As a Warrior, your primary role is to be the stalwart \
-    defender of your party, ensuring the safety of your comrades on the battlefield.\
-     You are a master of defensive skills and tactics,\
-      capable of withstanding even the most formidable foes.\
-       Your unwavering dedication to protection makes you an \
-       indispensable asset in any group."
+  out << "\nw-Warrior" << endl;
+  out << "Description: As a Warrior, your primary role is to be the stalwart \n"
+    "defender of your party, ensuring the safety of your comrades on the battlefield.\n"
+     "You are a master of defensive skills and tactics,\n"
+      "capable of withstanding even the most formidable foes.\n"
+       "Your unwavering dedication to protection makes you an \n"
+       "indispensable asset in any group."
       << endl;
   out << endl;
   out << "m-Mage" << endl;
-  out << "Description: As a Mage, you possess a unique combination of magical prowess and balance in your abilities.\
-     You harness the power of arcane energies to both damage enemies and support your allies.\
-      Your adaptability and versatility in the arcane arts make you an invaluable addition\
-       to any adventuring party."
+  out << "Description: As a Mage, you possess a unique combination of magical prowess and balance in your abilities.\n"
+     "You harness the power of arcane energies to both damage enemies and support your allies.\n"
+      "Your adaptability and versatility in the arcane arts make you an invaluable addition\n"
+       "to any adventuring party."
       << endl;
   out << endl;
   out << "s-Samurai" << endl;
-  out << "Description: The Samurai is a master of offense, wielding a blade with precision and skill.\
-     As a Samurai, your primary focus is to unleash powerful and devastating attacks on your enemies.\
-      Your offensive nature makes you a formidable force on the battlefield,\
-       and your precision ensures that your strikes find their mark."
+  out << "Description: The Samurai is a master of offense, wielding a blade with precision and skill.\n"
+     "As a Samurai, your primary focus is to unleash powerful and devastating attacks on your enemies.\n"
+      "Your offensive nature makes you a formidable force on the battlefield,\n"
+       "and your precision ensures that your strikes find their mark."
       << endl;
 }
 
 void Chapter1Part1::chooseClass() {
-  out << "What kind of job class you want?" << endl;
+  out << "What character type would you like?" << endl;
   jobDescription();
   std::getline(in, input);
   checkInput(in);
@@ -51,15 +51,15 @@ void Chapter1Part1::chooseClass() {
     std::getline(in, input);
   }
   if (input == "w") { // warrior not complete yet
-    //out << "You have choosed Warrior as your class." << endl;
+    //out << "\nYou have chosen Warrior as your class." << endl;
     //Warrior *w = new Warrior(name);
     //player = w;
   } else if (input == "m") {
-    out << "You have choosed Mage as your class." << endl;
+    out << "\nYou have chosen Mage as your class." << endl;
     Mage *m = new Mage(name);
     player = m;
   } else {
-    out << "You have choosed Samurai as your class." << endl;
+    out << "\nYou have chosen Samurai as your class." << endl;
     Samurai *s = new Samurai(name);
     player = s;
   }
@@ -69,9 +69,9 @@ void Chapter1Part1::runChapter1Part1Sub1() {
   string typedName;
   string cls;
 
-  out << "Game start!" << endl;
-  out << "Chapter1" << endl;
-  out << "What is your character's name?(Do not contain your name with space)"
+  out << "\nGame start!" << endl;
+  out << "Chapter 1 Begins..." << endl;
+  out << "\nWhat is your character's name? (Do not use any spaces within your name.)"
       << endl;
   std::getline(in, name);
   checkInput(in);
--- test/testCh1Part1.cpp
+++ test/testCh1Part1.cpp
@@ -36,27 +36,27 @@ TEST(TestCH1Part1Suite, TestjobDescription) // test if output are equal
   Character * c = new Character("test");
   Chapter1Part1 *ch11 = new Chapter1Part1(c, testout, testin);
   
-  out << "w-Warrior" << endl;
-  out << "Description: As a Warrior, your primary role is to be the stalwart \
-    defender of your party, ensuring the safety of your comrades on the battlefield.\
-     You are a master of defensive skills and tactics,\
-      capable of withstanding even the most formidable foes.\
-       Your unwavering dedication to protection makes you an \
-       indispensable asset in any group."
+  out << "\nw-Warrior" << endl;
+  out << "Description: As a Warrior, your primary role is to be the stalwart \n"
+    "defender of your party, ensuring the safety of your comrades on the battlefield.\n"
+     "You are a master of defensive skills and tactics,\n"
+      "capable of withstanding even the most formidable foes.\n"
+       "Your unwavering dedication to protection makes you an \n"
+       "indispensable asset in any group."
       << endl;
   out << endl;
   out << "m-Mage" << endl;
-  out << "Description: As a Mage, you possess a unique combination of magical prowess and balance in your abilities.\
-     You harness the power of arcane energies to both damage enemies and support your allies.\
-      Your adaptability and versatility in the arcane arts make you an invaluable addition\
-       to any adventuring party."
+  out << "Description: As a Mage, you possess a unique combination of magical prowess and balance in your abilities.\n"
+     "You harness the power of arcane energies to both damage enemies and support your allies.\n"
+      "Your adaptability and versatility in the arcane arts make you an invaluable addition\n"
+       "to any adventuring party."
       << endl;
   out << endl;
   out << "s-Samurai" << endl;
-  out << "Description: The Samurai is a master of offense, wielding a blade with precision and skill.\
-     As a Samurai, your primary focus is to unleash powerful and devastating attacks on your enemies.\
-      Your offensive nature makes you a formidable force on the battlefield,\
-       and your precision ensures that your strikes find their mark."
+  out << "Description: The Samurai is a master of offense, wielding a blade with precision and skill.\n"
+     "As a Samurai, your primary focus is to unleash powerful and devastating attacks on your enemies.\n"
+      "Your offensive nature makes you a formidable force on the battlefield,\n"
+       "and your precision ensures that your strikes find their mark."
       << endl;
 
   ch11->jobDescription();
@@ -73,30 +73,30 @@ TEST(TestCH1Part1Suite, testChooseClassMage) {
 
   testin << "m" << endl; // choose mage
 
-  out << "What kind of job class you want?" << endl;
-  out << "w-Warrior" << endl;
-  out << "Description: As a Warrior, your primary role is to be the stalwart \
-    defender of your party, ensuring the safety of your comrades on the battlefield.\
-     You are a master of defensive skills and tactics,\
-      capable of withstanding even the most formidable foes.\
-       Your unwavering dedication to protection makes you an \
-       indispensable asset in any group."
+  out << "What character type would you like?" << endl;
+  out << "\nw-Warrior" << endl;
+  out << "Description: As a Warrior, your primary role is to be the stalwart \n"
+    "defender of your party, ensuring the safety of your comrades on the battlefield.\n"
+     "You are a master of defensive skills and tactics,\n"
+      "capable of withstanding even the most formidable foes.\n"
+       "Your unwavering dedication to protection makes you an \n"
+       "indispensable asset in any group."
       << endl;
   out << endl;
   out << "m-Mage" << endl;
-  out << "Description: As a Mage, you possess a unique combination of magical prowess and balance in your abilities.\
-     You harness the power of arcane energies to both damage enemies and support your allies.\
-      Your adaptability and versatility in the arcane arts make you an invaluable addition\
-       to any adventuring party."
+  out << "Description: As a Mage, you possess a unique combination of magical prowess and balance in your abilities.\n"
+     "You harness the power of arcane energies to both damage enemies and support your allies.\n"
+      "Your adaptability and versatility in the arcane arts make you an invaluable addition\n"
+       "to any adventuring party."
       << endl;
   out << endl;
   out << "s-Samurai" << endl;
-  out << "Description: The Samurai is a master of offense, wielding a blade with precision and skill.\
-     As a Samurai, your primary focus is to unleash powerful and devastating attacks on your enemies.\
-      Your offensive nature makes you a formidable force on the battlefield,\
-       and your precision ensures that your strikes find their mark."
+  out << "Description: The Samurai is a master of offense, wielding a blade with precision and skill.\n"
+     "As a Samurai, your primary focus is to unleash powerful and devastating attacks on your enemies.\n"
+      "Your offensive nature makes you a formidable force on the battlefield,\n"
+       "and your precision ensures that your strikes find their mark."
       << endl;
-  out << "You have choosed Mage as your class." << endl;
+  out << "\nYou have chosen Mage as your class." << endl;
 
   EXPECT_NO_THROW(ch11->chooseClass());
   EXPECT_EQ( testout.str(), out.str());
@@ -111,30 +111,30 @@ TEST(TestCH1Part1Suite, testChooseClassSamurai) {
 
   testin << "s" << endl; // choose mage
 
-  out << "What kind of job class you want?" << endl;
-  out << "w-Warrior" << endl;
-  out << "Description: As a Warrior, your primary role is to be the stalwart \
-    defender of your party, ensuring the safety of your comrades on the battlefield.\
-     You are a master of defensive skills and tactics,\
-      capable of withstanding even the most formidable foes.\
-       Your unwavering dedication to protection makes you an \
-       indispensable asset in any group."
+  out << "What character type would you like?" << endl;
+  out << "\nw-Warrior" << endl;
+  out << "Description: As a Warrior, your primary role is to be the stalwart \n"
+    "defender of your party, ensuring the safety of your comrades on the battlefield.\n"
+     "You are a master of defensive skills and tactics,\n"
+      "capable of withstanding even the most formidable foes.\n"
+       "Your unwavering dedication to protection makes you an \n"
+       "indispensable asset in any group."
       << endl;
   out << endl;
   out << "m-Mage" << endl;
-  out << "Description: As a Mage, you possess a unique combination of magical prowess and balance in your abilities.\
-     You harness the power of arcane energies to both damage enemies and support your allies.\
-      Your adaptability and versatility in the arcane arts make you an invaluable addition\
-       to any adventuring party."
+  out << "Description: As a Mage, you possess a unique combination of magical prowess and balance in your abilities.\n"
+     "You harness the power of arcane energies to both damage enemies and support your allies.\n"
+      "Your adaptability and versatility in the arcane arts make you an invaluable addition\n"
+       "to any adventuring party."
       << endl;
   out << endl;
   out << "s-Samurai" << endl;
-  out << "Description: The Samurai is a master of offense, wielding a blade with precision and skill.\
-     As a Samurai, your primary focus is to unleash powerful and devastating attacks on your enemies.\
-      Your offensive nature makes you a formidable force on the battlefield,\
-       and your precision ensures that your strikes find their mark."
+  out << "Description: The Samurai is a master of offense, wielding a blade with precision and skill.\n"
+     "As a Samurai, your primary focus is to unleash powerful and devastating attacks on your enemies.\n"
+      "Your offensive nature makes you a formidable force on the battlefield,\n"
+       "and your precision ensures that your strikes find their mark."
       << endl;
-  out << "You have choosed Samurai as your class." << endl;
+  out << "\nYou have chosen Samurai as your class." << endl;
 
   EXPECT_NO_THROW(ch11->chooseClass());
   EXPECT_EQ( testout.str(), out.str());
@@ -150,34 +150,34 @@ TEST(TestCH1Part1Suite, testrunChapter1Part1Sub1ChooseMageClass) {
   testin << "tester"<< endl;
   testin << "m" << endl;
 
-  out << "Game start!" << endl;
-  out << "Chapter1" << endl;
-  out << "What is your character's name?(Do not contain your name with space)"
+  out << "\nGame start!" << endl;
+  out << "Chapter 1 Begins..." << endl;
+  out << "\nWhat is your character's name? (Do not use any spaces within your name.)"
       << endl;
-  out << "What kind of job class you want?" << endl;
-  out << "w-Warrior" << endl;
-  out << "Description: As a Warrior, your primary role is to be the stalwart \
-    defender of your party, ensuring the safety of your comrades on the battlefield.\
-     You are a master of defensive skills and tactics,\
-      capable of withstanding even the most formidable foes.\
-       Your unwavering dedication to protection makes you an \
-       indispensable asset in any group."
+  out << "What character type would you like?" << endl;
+  out << "\nw-Warrior" << endl;
+  out << "Description: As a Warrior, your primary role is to be the stalwart \n"
+    "defender of your party, ensuring the safety of your comrades on the battlefield.\n"
+     "You are a master of defensive skills and tactics,\n"
+      "capable of withstanding even the most formidable foes.\n"
+       "Your unwavering dedication to protection makes you an \n"
+       "indispensable asset in any group."
       << endl;
   out << endl;
   out << "m-Mage" << endl;
-  out << "Description: As a Mage, you possess a unique combination of magical prowess and balance in your abilities.\
-     You harness the power of arcane energies to both damage enemies and support your allies.\
-      Your adaptability and versatility in the arcane arts make you an invaluable addition\
-       to any adventuring party."
+  out << "Description: As a Mage, you possess a unique combination of magical prowess and balance in your abilities.\n"
+     "You harness the power of arcane energies to both damage enemies and support your allies.\n"
+      "Your adaptability and versatility in the arcane arts make you an invaluable addition\n"
+       "to any adventuring party."
       << endl;
   out << endl;
   out << "s-Samurai" << endl;
-  out << "Description: The Samurai is a master of offense, wielding a blade with precision and skill.\
-     As a Samurai, your primary focus is to unleash powerful and devastating attacks on your enemies.\
-      Your offensive nature makes you a formidable force on the battlefield,\
-       and your precision ensures that your strikes find their mark."
+  out << "Description: The Samurai is a master of offense, wielding a blade with precision and skill.\n"
+     "As a Samurai, your primary focus is to unleash powerful and devastating attacks on your enemies.\n"
+      "Your offensive nature makes you a formidable force on the battlefield,\n"
+       "and your precision ensures that your strikes find their mark."
       << endl;
-  out << "You have choosed Mage as your class." << endl;
+  out << "\nYou have chosen Mage as your class." << endl;
 
   EXPECT_NO_THROW(ch11->runChapter1Part1Sub1());
 
@@ -197,7 +197,7 @@ TEST(TestCH1Part1Suite, testOption1) {
 
   out << "[Select Option]" << endl;
   out << "1.  Stop right there, don\'t go anywhere near that girl!" << endl
-      << "(Silently approach the bandit.)" << endl;
+      << "    (Silently approach the bandit.)" << endl;
 
   EXPECT_NO_THROW(ch11->option1Part1Sub1());
   EXPECT_EQ( testout.str(), out.str()); 
