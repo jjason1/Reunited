7c82513b27538d83956a71610184953b7a83dfee
bli181 <bli181@ucr.edu>
2023-12-04 00:37:19 -0800

    No fix

---
+++ lib/Enemies/Golem.hpp
@@ -0,0 +1,26 @@
+#ifndef GOLEM_HPP
+#define GOLEM_HPP
+
+
+#include <vector>
+#include "../../lib/Enemies/Enemy.hpp"
+#include "../../lib/Characters/Character.hpp"
+using namespace std;
+
+class Golem : public Enemy {
+
+
+    public:
+        int CD = 0;
+        Golem(string name, int level);
+        void lazerBlaze(Character *);
+        void earthquake(Character *);
+        void shieldGen();
+
+        virtual int selectSkill(istream &);
+        virtual void useSkillOn(int, Character *);
+
+
+};
+
+#endif /* GOLEM_HPP */
