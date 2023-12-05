8e20e0d87301ca7b0b4cdb8fad8f4006e835f7d1
Aamir Khan <aamirksfg@gmail.com>
2023-12-04 20:32:46 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,4 @@
 int main(int argv, char** argc) {
     //Set the random number seed.
     srand(static_cast<unsigned int>(time(0)));
-    StartNewGame *sng=new StartNewGame(cout,cin);
-    sng->start();
 }
